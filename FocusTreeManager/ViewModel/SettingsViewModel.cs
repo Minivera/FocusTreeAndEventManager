17f15300d85895d7460f29ac785509e01892d970
Shazbot <tilic5@gmail.com>
2017-07-05 07:36:18 +0200

    reload game images on game path change

--- FocusTreeManager/ViewModel/SettingsViewModel.cs
+++ FocusTreeManager/ViewModel/SettingsViewModel.cs
@@ -47,6 +47,7 @@ namespace FocusTreeManager.ViewModel
             {
                 Configurator.setGamePath(value);
                 RaisePropertyChanged(() => GamePath);
+                AsyncImageLoader.AsyncImageLoader.Worker.RestartStarterWorker();
             }
         }
 
@@ -73,7 +74,7 @@ namespace FocusTreeManager.ViewModel
         public SettingsViewModel()
         {
             AvailableLanguages = Configurator.returnAllLanguages();
-            selectedLanguage = AvailableLanguages.SingleOrDefault(l => l.FileName == 
+            selectedLanguage = AvailableLanguages.SingleOrDefault(l => l.FileName ==
                 Configurator.getLanguage());
             FindCommand = new RelayCommand(SelectGameFolder);
             if (!Configurator.getFirstStart())
